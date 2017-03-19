dfeb4b48e3e9c926176772e085067db06be78820
Gustavo Martínez <tavo962@gmail.com>
2017-03-19 08:56:16 -0600

    Titles and descriptions

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<?php
+$data = ( object ) [
+	'description'	=>	null,
+	'file'			=>	basename( $_SERVER[ 'SCRIPT_NAME' ] ),
+	'title'			=>	null,
+];
+
+switch( $data -> file ) {
+	case 'about-us.php':
+		$data -> title = 'About Us';
+		$data -> description = 'Description';
+		break;
+	
+	default:
+		$data -> title = 'Index';
+		$data -> description = 'Description';
+}	// end switch
+?>
 <!DOCTYPE html>
 <html lang="es-MX">
 	<head>
-		<title></title>
-
-		<meta charset="utf-8" />
+		<title><?php echo $data -> title;?></title>
 
 		<?php include 'structure/head.php';?>
 	</head>
@@ -11,7 +27,7 @@
 	<body>
 		<header><?php include 'structure/header.php';?></header>
 		<nav><?php include 'structure/navigation.php';?></nav>
-		<main><?php include 'structure/content/' . basename( $_SERVER[ 'SCRIPT_NAME' ] );?></main>
+		<main><?php include 'structure/content/' . $data -> file;?></main>
 		<footer><?php include 'structure/footer.php';?></footer>
 
 		<?php include 'structure/scripts.php';?>
--- structure/head.php
+++ structure/head.php
@@ -1,2 +1,7 @@
+<!-- Metadata -->
+<meta charset="utf-8" />
+<meta name="description" content="<?php echo $data -> description;?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 <!-- Styles -->
-<link href="" rel="stylesheet">
+<link href="styles.min.css" rel="stylesheet" />
