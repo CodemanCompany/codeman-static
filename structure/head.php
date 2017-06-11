6b8f9f208e0da9b169fe488122d67594da44dae3
Gustavo Martínez <tavo962@gmail.com>
2017-06-11 11:44:48 -0500

    Productivity

--- structure/head.php
+++ structure/head.php
@@ -1,7 +1,7 @@
 <!-- Metadata -->
 <meta charset="utf-8" />
 <meta name="description" content="<?php echo $data -> description;?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
 <!-- Styles -->
 <link href="css/styles.min.css" rel="stylesheet" />
