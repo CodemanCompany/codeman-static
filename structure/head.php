c8c204fb382d7852bcbeb2170331cc05326af1f7
Gustavo Martínez <gustavo@codeman.company>
2017-08-08 14:42:47 -0500

    Open Graph

--- structure/head.php
+++ structure/head.php
@@ -3,5 +3,10 @@
 <meta name="description" content="<?php echo $data -> description;?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
+<meta property="og:description" content="<?php echo $data -> description;?>" />
+<meta property="og:image" content="" />
+<meta property="og:title" content="<?php echo $data -> title;?>" />
+<meta property="og:url" content="" />
+
 <!-- Styles -->
 <link href="css/styles.min.css" rel="stylesheet" />
