56051d1ed16babf666f60f4f6b6d3145f3581515
Gustavo Martínez <gustavo@codeman.company>
2017-09-05 15:53:44 -0500

    Open Graph

--- structure/head.php
+++ structure/head.php
@@ -3,6 +3,8 @@
 <meta name="description" content="<?php echo $data -> description;?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
+<!-- <meta property="fb:app_id" content="" /> -->
+<meta property="og:type" content="website" />
 <meta property="og:description" content="<?php echo $data -> description;?>" />
 <meta property="og:image" content="" />
 <meta property="og:title" content="<?php echo $data -> title;?>" />
