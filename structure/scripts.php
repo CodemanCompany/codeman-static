56ab0da42a6cb4248ecfe49381fea497e906e2ae
Gustavo Martínez <tavo962@gmail.com>
2017-09-22 13:06:45 -0500

    NPM

--- structure/scripts.php
+++ structure/scripts.php
@@ -1,7 +1,7 @@
 <!-- Scripts -->
 <script async="async" src="https://www.google.com/recaptcha/api.js"></script>
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="js/main.js"></script>
 
 <!-- Google Analytics -->
