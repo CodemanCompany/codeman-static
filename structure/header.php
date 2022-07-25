af40ab4438d43762000edf29d173c9eb55bc28cf
“Roger84” <codeman@Mac-mini-de-Codeman.local>
2022-07-25 18:47:53 -0500

    Progress

--- structure/header.php
+++ structure/header.php
@@ -1,3 +1,26 @@
-<div class="container">
-	<!-- Write content -->
+<div class="header_danzon container pt-3">
+	<img src="/img/index/logo.png" alt="logo">
+		
+	<div style="width:50%">
+		<div class="d-flex">
+			<input type="search" id="form1" class="form-control" placeholder="Buscar" />
+			<button type="button" class="btn btn-primary">
+				<i class="fas fa-search"></i>
+			</button>
+		</div>
+	</div>
+	<div class="text-center">
+		<div class="d-contents">
+			<img class="user-icon" src="/img/index/icons/user-24.ico" alt="login">
+			<p>Inicia sesión <br> ó registratge</p>
+		</div>		
+		<div class="d-contents">
+			<img class="cart-icon" src="/img/index/icons/cart-59-24.ico" alt="cart">
+			<p>Carrito</p>
+		</div>		
+			<img style="max-width:28px" src="/img/index/icons/whatsapp.png" alt="whats">
+			<img style="max-width:24px" src="/img/index/icons/facebook.png" alt="Face">
+			<img style="max-width:24px" src="/img/index/icons/instagram.png" alt="Insta">
+		</div>
+	</div>
 </div>
