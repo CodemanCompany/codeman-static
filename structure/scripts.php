304933848a1bd9a6e5e51e49b490a01e8ce2435e
Gustavo Martínez <gustavo@codeman.company>
2022-07-26 13:40:08 -0500

    Progress Darwin

--- structure/scripts.php
+++ structure/scripts.php
@@ -11,16 +11,6 @@
 <script src="/js/main.js"></script>
 
 <!-- Codeman: Contact Kit -->
-<!-- <script src="/node_modules/angular/angular.min.js"></script>
-<script src="/js/lib/yaokiski.js"></script>
-<script src="/js/app.js"></script>
-<script src="/js/components.js"></script> -->
+<!-- <script src="/js/contact.js"></script> -->
 
 <!-- Google Analytics -->
-<!-- <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create','CODEMAN_CODE_HERE','auto');ga('send','pageview');
-</script> -->
